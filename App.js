9b665140d68dd48cfdd9848c0c8f36dab810a08e
wxlaryy <133116631+wxlaryy@users.noreply.github.com>
2024-10-04 12:42:38 +0000

    alterações

--- App.js
+++ App.js
@@ -1,3 +1,36 @@
 function criaCartao(categoria, pergunta, resposta) {
-    console.log(categoria, pergunta, resposta)
+    let container = document.getElementById('container')
+    let cartao = document.createElement('article')
+    cartao.className = 'cartao'
+
+    cartao.innerHTML =
+    <div class="cartao__conteudo">
+        <h3>${pergunta}</h3>
+        <div class="cartao__conteudo__pergunta">
+            <p>${pergunta}</p>
+        </div>
+        <div class ="cartao__conteudo__resposta">
+            <p>${resposta}</p>
+        </div>
+    </div>
+    ,
+
+
+    let respostaEstaVisivel = false
+
+    function viraCartao() {
+        respostaEstaVisivel = !respostaEstaVisivel
+        cartao.classList.toggle('active' , viraCartao)
+    }
+    cartao.addEventListener('click' , viraCartao)
+
+
+
+    container.appendChild(cartao)
+
+}
+    
+
+    container.appendChild(cartao)
+
 }
--- perguntas.js
+++ perguntas.js
@@ -15,3 +15,15 @@ criaCartao(
     'O que é uma função?',
     'Uma função é um bloco de código que executa alguma tarefa'
 )
+
+criaCartao(
+    'Programação',
+    'O que é uma função?'
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz oi em Inglês?'
+    'Oi em Ingles é HI (RAI)'
+)
