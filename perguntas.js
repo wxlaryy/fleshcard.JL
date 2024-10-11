3d828cd8a83813e6f5944537d7d92ce72fd988bc
wxlaryy <133116631+wxlaryy@users.noreply.github.com>
2024-10-11 12:37:19 +0000

    alterações

--- perguntas.js
+++ perguntas.js
@@ -12,12 +12,13 @@ criaCartao(
 
 criaCartao(
     'Programação',
-    'O que é uma função?'
-    'Uma função é um bloco de código que executa alguma tarefa'
+    'O que é uma função?',
+    "Uma função é um bloco de código que executa alguma tarefa"
 )
 
 criaCartao(
     'Lingua inglesa',
-    'Como se diz oi em Inglês?'
-    'Oi, em ingles é HI (RAI)'
+    'Como se diz oi em Inglês?',
+    "Oi em ingles é HI (RAI)"
+
 )
